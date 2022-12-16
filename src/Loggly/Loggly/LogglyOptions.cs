dcf17c3098b94305131cfb87901a4d256279a3cc
STOCKHOLM\dorin.ciobanu <dorin.ciobanu@educations.com>
2022-12-16 15:03:59 +0100

    re #18251_Issue_with_logging_of_IDs_as_integers_in_loggly
    - Have added an overridable SerializerSettings property to the LogglyOptions class with a default value
    - Have added a JsonConverter that would convert integer Id properties to string and included it in the default SerializerSettings
    - Changed the serializer settings to include Default Value properties

--- src/Loggly/Loggly/LogglyOptions.cs
+++ src/Loggly/Loggly/LogglyOptions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace EMG.Extensions.Logging.Loggly
 {
@@ -26,6 +27,7 @@ namespace EMG.Extensions.Logging.Loggly
         public Action<LogglyMessage> PreProcessMessage { get; set; }
 
         public Encoding ContentEncoding { get; set; } = Encoding.UTF8;
+        public JsonSerializerSettings SerializerSettings { get; set; } = JsonSettings.SerializerSettings;
     }
 
     public delegate bool FilterDelegate(string categoryName, EventId eventId, LogLevel logLevel);
